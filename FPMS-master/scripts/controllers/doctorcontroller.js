6fcba7bb78b2d828d5718b05775045fd43806327
dperez91 <pilobolusgreen@gmail.com>
2016-10-02 14:41:41 -0400

    stuff

---
+++ FPMS-master/scripts/controllers/doctorcontroller.js
@@ -0,0 +1,21 @@
+var mainApp = angular.module("mainApp", []);
+
+mainApp.controller('doctorController', function($scope){
+prepareLocalStorageObject();
+});
+
+function prepareLocalStorageObject(){
+//alert(localStorage.doctors);
+}
+    
+function getAllDoctors(){
+
+}
+
+function getDoctor(){
+
+}
+
+function saveDoctor(){
+
+}
---
+++ FPMS-master/scripts/controllers/patientcontroller.js
@@ -0,0 +1,21 @@
+var mainApp = angular.module("mainApp", []);
+
+mainApp.controller('patientController', function($scope){
+
+});
+
+function prepareLocalStorageObject(){
+
+}
+
+function getAllPatients(){
+
+}
+
+function getPatient(){
+
+}
+
+function savePatient(){
+
+}
